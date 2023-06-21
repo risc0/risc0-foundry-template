6bfe746846969639f8840a69bbf8e65484f927b6
Parker Thompson <parker@risczero.com>
2023-06-20 18:26:47 -0700

    Refactor env vars for prover (#9)
    * Introduce new env var system and cleanup old code
    
    * PROVE -> PROVE_MODE
    
    * Moved to local exec
    
    * Migrate back to release-0.15 branch
    
    * Drop tokio

--- README.md
+++ README.md
@@ -54,12 +54,14 @@ forge test
 
 For testing with [proof] generation, which might take some time to complete, execute the following command instead:
 ```bash
-PROVE_LOCALLY=1 forge test
+PROVE_MODE=local forge test
 ```
 
 For offloading your [proof] requests to a local Bonsai instance, you can execute the tests as follows:
 ```bash
-BONSAI_ENDPOINT=http://localhost:8080 API_KEY=test_key forge test
+export BONSAI_API_URL='<URL>'
+export BONSAI_API_KEY='<KEY>'
+PROVE_MODE=bonsai forge test
 ```
 
 ## Project Structure
@@ -77,14 +79,14 @@ Below are the primary files in the project directory
 │   ├── BonsaiStarter.t.sol         // Tests for basic callback contract
 │   └── BonsaiStarterLowLevel.t.sol // Tests for low-level callback contract
 └── methods                         // [zkVM guest programs] are built here
-    ├── Cargo.toml                  
+    ├── Cargo.toml
     ├── build.rs                    // Instructions for the risc0-build rust crate
     ├── guest                       // A rust crate containing your [zkVM guest programs]
-    │   ├── Cargo.toml              
-    │   └── src                     
+    │   ├── Cargo.toml
+    │   └── src
     │       └── bin                 // Your [zkVM guest programs] live here
     │           └── fibonacci.rs    // Example [guest program] for fibonacci number calculation
-    └── src                         
+    └── src
         ├── main.rs                 // Glue binary for locally testing Bonsai applications
         └── lib.rs                  // Built RISC Zero guest programs are compiled into here
 ```
--- methods/src/main.rs
+++ methods/src/main.rs
@@ -32,49 +32,20 @@ struct Args {
     input: Option<String>,
 }
 
-fn prove_locally(elf: &[u8], input: Vec<u8>) -> Vec<u8> {
+fn prove_locally(elf: &[u8], input: Vec<u8>, prove: bool) -> Vec<u8> {
     let env = ExecutorEnv::builder().add_input(&input).build();
     let mut exec = Executor::from_elf(env, elf).expect("Failed to instantiate executor");
     let session = exec.run().expect("Failed to run executor");
-    // Locally prove resulting journal
-    if env::var("PROVE_LOCALLY").is_ok() {
+    if prove {
         session.prove().expect("Failed to prove session");
+        // eprintln!("Completed proof locally");
+    } else {
+        // eprintln!("Completed execution without a proof locally");
     }
     session.journal
 }
 
 const POLL_INTERVAL_SEC: u64 = 4;
-
-#[derive(serde::Deserialize)]
-pub struct AlphaRes {
-    pub alpha: bool,
-}
-
-async fn alpha_selector() -> bool {
-    if let Ok(backend) = env::var("BONSAI_BACKEND") {
-        backend == "alpha"
-    } else {
-        let endpoint = env::var("BONSAI_ENDPOINT").expect("Missing BONSAI_ENDPOINT env var");
-        let parts = endpoint.split('|').collect::<Vec<&str>>();
-        if parts.len() != 2 {
-            panic!("Invalid BONSAI_ENDPOINT env var format, expected: '<api_url>|<api_key'");
-        }
-        let api_key = parts[1];
-
-        let client = reqwest::Client::new();
-        let res: AlphaRes = client
-            .get("https://36c2brqrq4.execute-api.us-west-2.amazonaws.com/stage/alpha")
-            .header("x-api-key", api_key)
-            .send()
-            .await
-            .expect("Failed to get /alpha route")
-            .json()
-            .await
-            .expect("Failed to deserialize alpha response");
-
-        res.alpha
-    }
-}
 
 fn prove_alpha(elf: &[u8], input: Vec<u8>) -> Vec<u8> {
     let client = AlphaClient::from_env().expect("Failed to create client from env var");
@@ -113,6 +84,7 @@ fn prove_alpha(elf: &[u8], input: Vec<u8>) -> Vec<u8> {
                     .expect("Failed to download receipt");
                 let receipt: SessionRollupReceipt = bincode::deserialize(&receipt_buf)
                     .expect("Failed to deserialize SessionRollupReceipt");
+                // eprintln!("Completed proof on bonsai alpha backend!");
                 return receipt.journal;
             }
             _ => {
@@ -122,8 +94,7 @@ fn prove_alpha(elf: &[u8], input: Vec<u8>) -> Vec<u8> {
     }
 }
 
-#[tokio::main]
-pub async fn main() {
+fn main() {
     // Parse arguments
     let args = Args::parse();
     // Search list for requested binary name
@@ -143,17 +114,13 @@ pub async fn main() {
     let output_bytes = match &args.input {
         Some(input) => {
             let input = hex::decode(&input[2..]).expect("Failed to decode input");
-            match env::var("BONSAI_ENDPOINT") {
-                Ok(_) => {
-                    if alpha_selector().await {
-                        tokio::task::spawn_blocking(move || prove_alpha(guest_entry.elf, input))
-                            .await
-                            .expect("Failed to run alpha sub-task")
-                    } else {
-                        panic!("unsupported backend");
-                    }
-                }
-                Err(_) => prove_locally(guest_entry.elf, input),
+            let prover = env::var("PROVE_MODE").unwrap_or("".to_string());
+
+            match prover.as_str() {
+                "bonsai" => prove_alpha(guest_entry.elf, input),
+                "local" => prove_locally(guest_entry.elf, input, true),
+                "none" => prove_locally(guest_entry.elf, input, false),
+                _ => prove_locally(guest_entry.elf, input, false),
             }
         }
         None => Vec::from(bytemuck::cast::<[u32; 8], [u8; 32]>(guest_entry.image_id)),
