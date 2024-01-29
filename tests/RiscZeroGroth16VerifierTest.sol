69401f1e27a6b78b3ac7851c8352c9a374aa6570
capossele <angelocapossele@gmail.com>
2024-01-29 17:47:12 +0000

    update license

--- tests/RiscZeroGroth16VerifierTest.sol
+++ tests/RiscZeroGroth16VerifierTest.sol
@@ -1,19 +1,18 @@
 // Copyright 2024 RISC Zero, Inc.
 //
-// The RiscZeroGroth16Verifier is a free software: you can redistribute it
-// and/or modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation, either version 3 of the License,
-// or (at your option) any later version.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// The RiscZeroGroth16Verifier is distributed in the hope that it will be
-// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-// Public License for more details.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License along with
-// the RiscZeroGroth16Verifier. If not, see <https://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: Apache-2.0
 
 pragma solidity ^0.8.9;
 
@@ -42,11 +41,13 @@ contract RiscZeroGroth16VerifierTest is IRiscZeroVerifier, Groth16Verifier {
     }
 
     /// @inheritdoc IRiscZeroVerifier
-    function verify(bytes calldata seal, bytes32, /*imageId*/ bytes32 postStateDigest, bytes32 /*journalDigest*/ )
-        public
-        view
-        returns (bool)
-    {
+    function verify(
+        bytes calldata seal,
+        bytes32,
+        /*imageId*/
+        bytes32 postStateDigest,
+        bytes32 /*journalDigest*/
+    ) public view returns (bool) {
         // Require that the seal be specifically empty.
         // Reject if the caller may have sent a real seal.
         return CONTROL_ID_0 == 0 && CONTROL_ID_1 == 0 && seal.length == 0 && postStateDigest == bytes32(0);
