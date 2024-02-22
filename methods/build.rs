87cfd4468570dbb9671db4a75991b2622451e2a6
Victor Graf <victor@risczero.com>
2024-02-21 17:06:16 -0800

    add some comments

--- methods/build.rs
+++ methods/build.rs
@@ -17,14 +17,18 @@ use std::{collections::HashMap, env};
 use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
 use risc0_build_ethereum::generate_solidity_files;
 
+// Paths where the generated Solidity files will be written.
 const SOLIDITY_IMAGE_ID_PATH: &str = "../contracts/ImageID.sol";
 const SOLIDITY_ELF_PATH: &str = "../tests/Elf.sol";
 
 fn main() {
+    // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
+    // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
     let use_docker = env::var("RISC0_USE_DOCKER").ok().map(|_| DockerOptions {
         root_dir: Some("../".into()),
     });
 
+    // Generate Rust source files for the methods crate.
     let guests = embed_methods_with_options(HashMap::from([(
         "bonsai-starter-methods-guest",
         GuestOptions {
@@ -33,6 +37,7 @@ fn main() {
         },
     )]));
 
+    // Generate Solidity source files for use with Forge.
     let solidity_opts = risc0_build_ethereum::Options::default()
         .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
         .with_elf_sol_path(SOLIDITY_ELF_PATH);
