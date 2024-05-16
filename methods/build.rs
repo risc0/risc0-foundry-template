4aa23473d3a75c528533646b1a0be0e53405b28a
capossele <angelocapossele@gmail.com>
2024-05-16 18:03:18 +0100

    fix env import

--- methods/build.rs
+++ methods/build.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::HashMap;
+use std::{collections::HashMap, env};
 
 use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
 use risc0_build_ethereum::generate_solidity_files;
